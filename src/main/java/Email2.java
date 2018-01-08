829647f95f8ca0ae04d76ef6c85e8dc5b87b9e04
tm <tanminghaoniua26@163.com>
2018-01-08 16:28:27 +0800

    sadadadddddsssssa

---
+++ src/main/java/Email2.java
@@ -0,0 +1,81 @@
+import sun.applet.Main;
+
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * Created by 94063 on 2017/12/29.
+ */
+public class Email2 {
+    // 发件人的邮箱 密码
+    public static String myEmailzhanghao = "940638353@qq.com";
+    public static String myEmailmima = "rqyuntaehvapbeid";
+    // 发件人的smtp地址
+    public static String myEmailsmtpHost = "smtp.qq.com";
+    public static String receiveMailzhanghao = "940638353@qq.com";
+    public static void main(String[] args) throws Exception {
+        // 1. 创建参数配置, 用于连接邮件服务器的参数配置
+        Properties props = new Properties();                    // 参数配置
+        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", myEmailsmtpHost);   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
+
+        // PS: 某些邮箱服务器要求 SMTP 连接需要使用 SSL 安全认证 (为了提高安全性, 邮箱支持SSL连接, 也可以自己开启),
+        //     如果无法连接邮件服务器, 仔细查看控制台打印的 log, 如果有有类似 “连接失败, 要求 SSL 安全连接” 等错误,
+        //     打开下面 /* ... */ 之间的注释代码, 开启 SSL 安全连接。
+
+        // SMTP 服务器的端口 (非 SSL 连接的端口一般默认为 25, 可以不添加, 如果开启了 SSL 连接,
+        //                  需要改为对应邮箱的 SMTP 服务器的端口, 具体可查看对应邮箱服务的帮助,
+        //                  QQ邮箱的SMTP(SLL)端口为465或587, 其他邮箱自行去查看)
+        final String smtpPort = "465";
+        props.setProperty("mail.smtp.port", smtpPort);
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.socketFactory.port", smtpPort);
+
+
+        Session session = Session.getDefaultInstance(props);
+        // 设置Debug模式
+        session.setDebug(true);
+        // 创建一封电子邮件
+        MimeMessage message = createMimeMessage(session, myEmailzhanghao, receiveMailzhanghao);
+        // 根据Session获取邮件传输对象
+        Transport transport = session.getTransport();
+        transport.connect(myEmailzhanghao, myEmailmima);
+        // 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
+    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail) throws Exception {
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+
+        // 2. From: 发件人（昵称有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改昵称）
+        message.setFrom(new InternetAddress(sendMail, "TEXT测试邮件", "UTF-8"));
+
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "帅气先生", "UTF-8"));
+
+        // 4. Subject: 邮件主题（标题有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改标题）
+        message.setSubject("TEXT测试 不加附件", "UTF-8");
+
+        // 5. Content: 邮件正文（可以使用html标签）（内容有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改发送内容）
+        message.setContent("hello，我是不是贼拉拉帅   帅的不要不要的那种   TEXT。。。", "text/html;charset=UTF-8");
+
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+
+        // 7. 保存设置
+        message.saveChanges();
+
+        return message;
+
+
+    }
+
+
+}
